1a8416ff8cf1b8190d3cc44316328df308ca7f3c
mindaugaskadzys <mindaugas.kadzys@gmail.com>
2020-05-15 15:07:57 +0100

    pridetas php1 pirma uzduotis

---
+++ PHP1/index01.php
@@ -0,0 +1,20 @@
+<?php
+    $country = 'Lietuva';
+    $city = 'Vilnius';
+    $population = 574221;
+?>
+<!DOCTYPE html>
+    <html lang="lt">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP uzduotis nr.1</title>
+    </head>
+    <body>
+        <ul>
+            <li><?php echo $country ?></li>
+            <li><?php echo $city ?></li>
+            <li><?php echo $population ?></li>
+        </ul>
+    </body>
+</html>
